b881d1f1408b960b17ffa31dc67a74dc4b525588
Kiryu <kiryu.balbas@gmail.com>
2025-08-28 12:50:27 -0400

    ya no me ayudes ruben ya resolvi :D

--- src/routes/admin/route.tsx
+++ src/routes/admin/route.tsx
@@ -1,6 +1,6 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "../../components/ui/sidebar";
+import { AppSidebar } from "../../components/app-sidebar";
 
 export const Route = createFileRoute("/admin")({
   component: AdminLayoutComponent,
